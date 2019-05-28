f4e147193e58f42594ef0f9e936730aa63994cbb
jwpleow <woeltic@gmail.com>
2019-05-28 14:01:35 +0100

    Add wrtdrone conversion script

--- src/follower/src/gps_wrtdrone_conversion_node.cpp
+++ src/follower/src/gps_wrtdrone_conversion_node.cpp
@@ -41,26 +41,22 @@ void gps_callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
     g_gps_position_pub.publish(position_msg);
 }
 
-
-
-
 ///< get reference LLA from drone
 double latitude, longitude, altitude;
 bool connection = false;
-sensor_msgs::NavSatFix DroneGPS_{};
+sensor_msgs::NavSatFix droneGPS_{};
 
-void getDroneGPS_CB(const sensor_msgs::NavSatFix::ConstPtr& msg)
+void drone_ref_callback(const sensor_msgs::NavSatFix::ConstPtr& msg)
 {
-  DroneGPS_ = *msg;
+  droneGPS_ = *msg;
   connection = true;
-  longitude = DroneGPS_.longitude;
-  latitude = DroneGPS_.latitude;
-  altitude = DroneGPS_.altitude;
+  longitude = droneGPS_.longitude;
+  latitude = droneGPS_.latitude;
+  altitude = droneGPS_.altitude;
+
+  // initialise geodetic reference as this point (updates per subscriber Hz)
+  g_geodetic_converter.initialiseReference(latitude, longitude, altitude);
 }
-
-
-
-
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "gps_wrtdrone_conversion_node");
@@ -68,32 +64,23 @@ int main(int argc, char **argv) {
   ros::NodeHandle pnh("~");
   ros::Rate rate(10);
 
-  ///< Subscribe to Drone GPS Data for reference LLA
-  ros::Subscriber read_gps_sub_ = nh.subscribe<sensor_msgs::NavSatFix>("/mavros/global_position/global",10, getDroneGPS_CB);
+  // Subscribe to Drone GPS Data for reference LLA
+  ros::Subscriber read_gps_sub_ = nh.subscribe<sensor_msgs::NavSatFix>("/mavros/global_position/global",10, &drone_ref_callback);
   
   if (!connection) ROS_INFO("Waiting for GPS reference parameters...");
   while(!connection){
     ros::spinOnce();
-        rate.sleep();
+    rate.sleep();
   }
 
-  
+  ROS_INFO("GPS reference parameters retrieved.");
+
   // Specify whether covariances should be set manually or from GPS
   ros::param::param("~trust_gps", g_trust_gps, false);
 
   // Get manual parameters
   ros::param::param<std::string>("~frame_id",
                                  g_frame_id, "world");
-
-  // initialise GPS reference parameters
-  g_geodetic_converter.initialiseReference(latitude, longitude, altitude);
-
-  // Show reference point
-  double initial_latitude, initial_longitude, initial_altitude;
-  g_geodetic_converter.getReference(&initial_latitude, &initial_longitude,
-                                    &initial_altitude);
-  ROS_INFO("GPS reference initialized correctly %f, %f, %f", initial_latitude,
-           initial_longitude, initial_altitude);
 
   // Initialize publisher
   g_gps_position_pub =
